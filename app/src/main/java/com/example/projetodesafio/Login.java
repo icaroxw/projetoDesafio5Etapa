37ee275ec7194f0765a50585d0f13b02a5a2e094
Icaro <icaro.rocha1@gmail.com>
2023-10-03 15:26:59 -0300

    Update on login and register screen Add home page

---
+++ app/src/main/java/com/example/projetodesafio/Login.java
@@ -0,0 +1,37 @@
+package com.example.projetodesafio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+public class Login extends AppCompatActivity {
+
+    GoogleSignInClient googleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView textCreateAccount = findViewById(R.id.textCreateAccount);
+
+        textCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToRegister(view);
+            }
+        });
+
+    }
+
+    private void goToRegister(View view) {
+        Intent in = new Intent(Login.this, Register.class);
+        startActivity(in);
+    }
+
+}
--- app/src/main/java/com/example/projetodesafio/Register.java
+++ app/src/main/java/com/example/projetodesafio/Register.java
@@ -2,7 +2,10 @@ package com.example.projetodesafio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView textAlreadyHaveAccount = findViewById(R.id.textAlreadyHaveAccount);
+
+        textAlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin(view);
+            }
+        });
+    }
+
+    private void goToLogin(View view) {
+        Intent in = new Intent(Register.this, Login.class);
+        startActivity(in);
     }
 }
+
